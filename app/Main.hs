11f78bb6d66811bb76f500a3524c9189ab5a3acb
cuboktahedron <cuboktahedron@gmail.com>
2019-06-29 23:15:39 +0900

    init randomly

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import           Graphics.Gloss
 import           Graphics.Gloss.Data.ViewPort
 import           Debug.Trace
+import           System.Random
+import           Control.Monad
 
 -------------------
 -- Display の設定
@@ -21,19 +23,19 @@ window =
 -- シミュレーションの実装
 --------------------------
 cellWidth :: Float
-cellWidth = 50
+cellWidth = 5
 
 cellHeight :: Float
-cellHeight = 50
+cellHeight = 5
 
 fieldSize :: Int
-fieldSize = 5
+fieldSize = 100
 
 fieldSizeM1 :: Int
 fieldSizeM1 = fieldSize - 1
 
-initialCells :: [[Bool]]
-initialCells = replicate fieldSize $ replicate fieldSize True
+initialCells :: IO [[Bool]]
+initialCells = mapM (\_ -> replicateM fieldSize randomIO) [0 .. fieldSizeM1]
 
 drawCells :: [[Bool]] -> Picture
 drawCells cells =
@@ -44,7 +46,7 @@ drawCell :: [[Bool]] -> (Int, Int) -> Picture
 drawCell cells (x, y) =
   let coordX = cellWidth * fromIntegral x
       coordY = cellHeight * fromIntegral y
-      cell = cells !! x !! y
+      cell = cells !! y !! x
   in if cell
      then translate coordX coordY $ rectangleSolid cellWidth cellHeight
      else blank
@@ -53,9 +55,7 @@ nextCells :: ViewPort -> Float -> [[Bool]] -> [[Bool]]
 nextCells vp dt board =
   let coords = [(x, y) | x <- [0 .. fieldSizeM1], y <- [0 .. fieldSizeM1]]
       cells = map (nextCell board) coords
-      b = [take fieldSize (drop (fieldSize * i) cells)
-          | i <- [0 .. fieldSizeM1]]
-  in b
+  in [take fieldSize (drop (fieldSize * i) cells) | i <- [0 .. fieldSizeM1]]
 
 nextCell :: [[Bool]] -> (Int, Int) -> Bool
 nextCell board (x, y) =
@@ -68,6 +68,7 @@ nextCell board (x, y) =
           , isActive board (x + 1) (y - 1)
           , isActive board (x - 1) y
           , isActive board (x + 1) y
+          , isActive board (x - 1) (y + 1)
           , isActive board x (y + 1)
           , isActive board (x + 1) (y + 1)]
   in if cell
@@ -79,10 +80,12 @@ isActive board x y = let size = length board
                          outer = x < 0 || x >= size || y < 0 || y >= size
                      in if outer
                         then False
-                        else board !! y !! x
+                        else board !! x !! y
 
 -------------
 -- main 関数
 -------------
 main :: IO ()
-main = simulate window white 1 initialCells drawCells nextCells
+main = do
+  cells <- initialCells
+  simulate window white 10 cells drawCells nextCells
