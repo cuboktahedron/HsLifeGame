f62f65069f892e3d66edac65b66ecbf9bf7f6643
cuboktahedron <cuboktahedron@gmail.com>
2019-07-06 14:27:59 +0900

    make editable cell

--- app/Main.hs
+++ app/Main.hs
@@ -24,11 +24,16 @@ data State = State { cells :: [[Bool]]
                    , frame :: Int
                    , isPaused :: Bool
                    , isOneTime :: Bool
+                   , pt :: (Int, Int)
                    }
 
 defState :: State
-defState =
-  State { cells = [[]], frame = 0, isPaused = False, isOneTime = False }
+defState = State { cells = [[]]
+                 , frame = 0
+                 , isPaused = False
+                 , isOneTime = False
+                 , pt = (-1, -1)
+                 }
 
 --------------------------
 -- シミュレーションの実装
@@ -46,7 +51,7 @@ fieldSizeM1 :: Int
 fieldSizeM1 = fieldSize - 1
 
 initialCells :: IO State
-initialCells = 
+initialCells =
   let cells = map (\_ -> replicate fieldSize False) [0 .. fieldSizeM1]
   in return defState { cells = cells, isPaused = True }
 
@@ -67,7 +72,20 @@ drawState state =
                     }
   in applyViewPortToPicture vp
      $ Pictures
-     $ drawCells (cells state) ++ drawFrame (frame state)
+     $ drawCells (cells state)
+     ++ [drawCursor (pt state)]
+     ++ drawFrame (frame state)
+
+drawCursor :: (Int, Int) -> Picture
+drawCursor (x, y) =
+  if isOuter (x, y)
+  then blank
+  else translate (fromIntegral (x) * cellWidth) (fromIntegral (y) * cellHeight)
+    $ color red
+    $ rectangleSolid cellWidth cellHeight
+
+isOuter :: (Int, Int) -> Bool
+isOuter (x, y) = (x < 0 || x >= fieldSize || y < 0 || y >= fieldSize)
 
 drawCells :: [[Bool]] -> [Picture]
 drawCells cells =
@@ -144,7 +162,7 @@ isActive board x y =
 
 operateIO :: Event -> State -> IO State
 operateIO (EventKey key ks _ _) state = operateWithKeyIO key ks state
-operateIO (EventMotion _) state = return state
+operateIO (EventMotion pt) state = operateMotionIO pt state
 operateIO (EventResize _) state = return state
 
 operateWithKeyIO :: Key -> KeyState -> State -> IO State
@@ -152,19 +170,46 @@ operateWithKeyIO (MouseButton LeftButton) ks state = return
   $ if ks == Down
     then state { isOneTime = True }
     else state
+operateWithKeyIO (MouseButton RightButton) ks state = return
+  $ if ks == Down
+    then state { cells = (turnCell (pt state) (cells state))
+               , isPaused = True
+               , frame = 0
+               }
+    else state
 operateWithKeyIO (Char 'c') ks state =
   if ks == Down
-    then initialCells
-    else return state
+  then initialCells
+  else return state
 operateWithKeyIO (Char 'r') ks state =
   if ks == Down
-    then randomCells
-    else return state
+  then randomCells
+  else return state
 operateWithKeyIO (Char 's') ks state = return
   $ if ks == Down
     then state { isPaused = not (isPaused state) }
     else state
 operateWithKeyIO _ _ state = return state
+
+turnCell :: (Int, Int) -> [[Bool]] -> [[Bool]]
+turnCell (x, y) cells =
+  if isOuter (x, y)
+  then cells
+  else [if y == yy
+        then turnCell' (cells !! yy)
+        else cells !! yy
+       | yy <- [0 .. fieldSizeM1]]
+  where
+    turnCell' cs = [if x == xx
+                    then not (cs !! xx)
+                    else cs !! xx
+                   | xx <- [0 .. fieldSizeM1]]
+
+operateMotionIO :: (Float, Float) -> State -> IO State
+operateMotionIO (ptx, pty) state =
+  let x = round ((ptx + 320 - cellWidth) / cellWidth)
+      y = round ((pty + 320 - cellHeight) / cellHeight)
+  in return state { pt = (x, y) }
 
 -------------
 -- main 関数
